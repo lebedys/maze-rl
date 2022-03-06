094284955cccdf2b48c871ce8a532c20f15bcfb5
Adomas Lebedys <adomas.lebedys@protonmail.com>
2022-03-06 13:37:46 +0000

    add utility functions to display section of maze walls or fires in ASCII

---
+++ maze/util.py
@@ -0,0 +1,18 @@
+def walls_to_text(walls, nrows=3, ncols=3, row=0, col=0):
+    txt = ''
+    for row in walls[row:row+nrows,col:col+ncols]:
+        for cell in row:
+            mark = '-' if cell else 'x'
+            txt += mark + ' '
+        txt += '\n'
+
+    return txt
+
+def fires_to_text(fires, nrows=3, ncols=3, row=0, col=0):
+    txt = ''
+    for row in fires[:nrows,:ncols]:
+        for cell in row:
+            txt += str(cell) + ' '
+        txt += '\n'
+
+    return txt
