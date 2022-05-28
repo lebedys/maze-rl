e76f792377e469fde87fd5c32c013c70c6b0af53
Adomas Lebedys <adomas.lebedys@protonmail.com>
2022-05-28 17:04:21 +0100

    implement unoptimized agent; test on 9x9

--- maze/agent.py
+++ maze/agent.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import torch
 from typing import Tuple
@@ -33,17 +35,18 @@ N_ACTIONS = 5
 
 # default agent rewards:
 REWARDS = {
-    'finish': 10,  # finishing maze
+    'finish': 1.,  # finishing maze
 
     # obstacles:
-    'wall': -1.,  # hit wall
+    'wall': -0.1,  # hit wall
     'fire': -1.,  # hit fire
 
     # movement:
-    'step': -0.1,  # take step in any direction
-    'stay': -0.2,  # staying in place
-    'return': -0.5,  # take step backwards
+    'step_taken': -0.,  # take step in any direction
+    'stay': -0.,  # staying in place
+    'return': -0.,  # take step backwards
     'visited': 0,  # todo - penalize return to visited position
+    # todo - penalize path length?
 }
 
 
@@ -56,98 +59,122 @@ class Agent:
                  Q: np.ndarray = None,          # Q-table
                  rewards: dict = REWARDS,       # rewards
                  discount: float = 0.1,         # discount factor
-                 euclidian_cost_weighting: float = 0.0  # todo - explore impact of this parameter
+                 learning_rate: float = 1,
+                 euclidian_cost_weighting: float = 0.0,  # todo - explore impact of this parameter
                  ):
 
-        self.start_position = np.array(list(start_position))
-        self.end_position = np.array(list(end_position))
+        self.start_position = np.array(list(start_position), dtype=int)
+        self.end_position = np.array(list(end_position), dtype=int)
         self.Q = Q
         self.rewards = rewards
+        self.learning_rate = learning_rate
         self.discount = discount  # discount factor (gamma)
 
         self.euclidian_cost_weighting = euclidian_cost_weighting
 
         # initialize agent position:
-        self.position = start_position  # initial position
+        self.position = np.copy(self.start_position)  # initial position
+        self.step_count = 0
 
-        if Q is None:  # if no predefined Q-table provided
-            self.Q = np.full((WIDTH, HEIGHT, N_ACTIONS), 1. / N_ACTIONS)  # initialize default q values
+        if self.Q is None:  # if no predefined Q-table provided
+            self.Q = np.full((WIDTH, HEIGHT, N_ACTIONS), 1. / N_ACTIONS)  # initialize default q values equal for all
+            self.Q[1, 1, :] = np.random.rand(5)  # assign random q-values for start position
             # todo - add noise or some randomness
 
     def reset_position(self) -> None:
-        self.position = self.start_position  # initial position
+        self.position = np.copy(self.start_position) # initial position
 
     def is_finished(self) -> bool:
-        return self.position == self.end_position
+        is_finished = (self.position == self.end_position).all()
+        if is_finished: print('finished!')
+        return is_finished
 
     def observe(self, maze: np.ndarray) -> np.ndarray:
         # [[0, 1, 2],
         #  [3, 4, 5],
         #  [6, 7, 8]]
 
-        x, y = self.position
-        return maze[x - 1:x + 1, y - 1:y + 1]
+        row, col = self.position
+        return maze[row - 1: (row + 1) + 1, col - 1: (col + 1) + 1]
 
     def step(self,
-             observation: np.ndarray,  # local observation
-             train=False,  # enable training
-             epsilon: float = 0.,  # todo - exploration
-             q_noise: float = 0.,  # todo - random noise
+             walls: np.ndarray,  # local observation of walls
+             fires: np.ndarray,  # local observation of fires
+             train=False,        # enable training
+             epsilon: float = 0.1,  # todo - exploration
+             q_noise: float = 0.0,  # todo - random noise
              ) -> None:  # random exploration probability
 
         # todo - epsilon for random choice selection
         # todo - add random noise to q values
         # todo - invalidate some directions
 
-        if np.random.random() < epsilon:  # random exploration
-            chosen_direction = np.random.choice(directions)
+        # get Q(s) vector:
+        row, col = self.position
+        q_values = self.Q[row, col, :]
+
+        # if np.random.random() < epsilon:  # random exploration
+        if False:
+            chosen_q_index = np.random.randint(0, 5)
+            # fixme - need q index chosen
         else:
-            # get Q(s) vector:
-            q_values = self.Q[self.position[0], self.position[1], :]
-
             # todo - random noise applied to q value vector
-
             # pick maximum q-value from Q(s):
-            max_q = np.max(q_values)  # maximum q value
-            chosen_direction = directions[np.argmax(q_values)]  # select corresponding direction
+            chosen_q_index = np.argmax(q_values)  # select index of highest q-value
 
-        reward = self.rewards['step']
+        chosen_q = q_values[chosen_q_index]  # corresponding q value
+        chosen_direction = directions[chosen_q_index]  # choose corresponding direction
+
+        reward = self.rewards['step_taken']  # add initial penalty for making any action
+        # todo - should this be applied even when not moving?
 
         # flatten observation: [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        walls, fires = observation
-        walls = walls.flatten()
-        fires = fires.flatten()
+        # walls, fires = observation
+        # walls = walls.flatten()
+        # fires = fires.flatten()
 
-        next_position = self.position  # initialize next position calculation
+        next_row, next_col = row, col
         if chosen_direction == Direction.NONE:  # no change in position
             reward += self.rewards['stay']  # penalty for staying in place
+            next_cell = walls[1, 1]
         elif chosen_direction == Direction.UP:
-            next_position[1] -= 1  # move up
+            next_row -= 1  # move up
+            next_cell = walls[0, 1]
         elif chosen_direction == Direction.RIGHT:
-            next_position[0] += 1  # move right
+            next_col += 1  # move right
+            next_cell = walls[1, 2]
         elif chosen_direction == Direction.DOWN:
-            next_position[1] += 1  # move down
+            next_row += 1  # move down
+            next_cell = walls[2, 1]
         elif chosen_direction == Direction.LEFT:
-            next_position[1] -= 1  # move left
+            next_col -= 1  # move left
+            next_cell = walls[1, 0]
         else:
-            raise ValueError()  # fixme - correct exception type needed
+            raise ValueError('Directionality value not valid.')
 
-        # do not allow walking through obstacles:
-        if is_wall(chosen_direction, walls):  # penalize wall hit
+        # penalize hitting obstacles:
+        # next_cell = walls[next_row-row, next_col-col]
+        if is_wall(next_cell):    # penalize wall hit
             reward += self.rewards['wall']
-        elif is_fire(chosen_direction, fires):  # penalize fire hit
+            next_row, next_col = row, col  # revert position
+        elif is_fire(next_cell):  # penalize fire hit
             reward += self.rewards['fire']
-        else:
-            self.position = next_position
+            next_row, next_col = row, col  # revert position
 
-        previous_position = 0
-        if next_position == previous_position:
-            pass  # todo - check and penalize if returning to previous move
+        # previous_position = 0
+        # if (next_position == previous_position).all():
+        #     pass  # todo - check and penalize if returning to previous move
 
-        # small negative reward for distance to goal
-        reward += euclidian_cost(self.position, self.end_position) * self.euclidian_cost_weighting
+        # todo - small negative reward for distance to goal
+        # reward += euclidian_cost(self.position, self.end_position) * self.euclidian_cost_weighting
 
-        # todo - update qtable
+        # estimate of maximum future q-value:
+        max_q = self.Q[next_row, next_col, :].max()
+
+        self.Q[row, col, chosen_q_index] += \
+            self.learning_rate * (reward - chosen_q + self.discount * max_q)
+
+        self.position = np.array([next_row, next_col])  # update position
 
     def train(self,
               maze: np.ndarray,
@@ -157,17 +184,19 @@ class Agent:
         self.reset_position()  # initialize agent
 
         # initialize training path:
-        train_path = np.empty(max_steps, 2)  # empty path
-        train_path[0, :] = self.position  # add initial position
+        train_path = np.empty(shape=(max_steps+1, 2), dtype=int)  # empty path
+        train_path[0, :] = np.copy(self.position)  # add initial position
 
         for i in range(max_steps):
             observation = self.observe(maze)  # get surrounding information
+            walls = observation
+            fires = np.array([])
 
-            self.step(observation, train=True)  # update position
-            train_path[i] = self.position  # store new position
+            self.step(walls=walls, fires=fires, train=True)  # update position
+            train_path[i+1, :] = np.copy(self.position)  # store new position
 
             if self.is_finished():  # achieved goal?
-                train_path = train_path[:i+1, :]  # truncate if path unfilled before returning
+                train_path = train_path[:i+2, :]  # truncate if path unfilled before returning
                 break  # end training
 
         return train_path  # return path when finished
@@ -175,7 +204,7 @@ class Agent:
     def run(self, maze, max_steps=10):  # run pretrained agent
         self.reset_position()  # initialize agent
 
-        run_path = np.empty(max_steps, 2)
+        run_path = np.empty(shape=(max_steps+1, 2), dtype=int)
         run_path[0, :] = self.position  # add initial position
 
         for i in range(max_steps):
--- maze/run.py
+++ maze/run.py
@@ -1,16 +1,38 @@
+import maze.mazes.sample_mazes
 from maze.lib import read_maze
 import agent
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import display.display as dp
 
 import numpy as np
 
 read_maze.load_maze('./mazes/final.npy')
 walls = read_maze.maze_cells[:, :, 0]
 
+walls_9 = maze.mazes.sample_mazes.sample_maze_9_A  # 9x9 test maze
+
+wall_color = '#eeeeee'
+path_color = '#111111'
+
 if __name__ == '__main__':
-    a0 = agent.Agent()  # instantiate new agent
-    p0 = a0.run(walls)
 
-    # todo - plot untrained agent path
+    a0 = agent.Agent(
+        end_position=(7, 7)
+    )  # instantiate new agent
 
-    a1 = a0.train()  # todo - train agent with one run
-    p1 = a1.run()  # todo - run trained agent
+    train_path = a0.train(maze=walls_9, max_steps=300)
+
+    fig, ax = plt.subplots()
+    dp.plot_maze_walls(walls_9,
+                       ax=ax,
+                       cmap=ListedColormap([path_color, wall_color])
+                       )
+
+    dp.plot_agent_path(train_path, shape=(9, 9),
+                       ax=ax,
+                       cmap=ListedColormap(['none', 'red'])
+                       )
+
+    plt.show()
