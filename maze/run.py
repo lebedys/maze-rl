7ebbcb456e6be0a86074b95d139a667a5fed512e
Adomas Lebedys <adomas.lebedys@protonmail.com>
2022-05-28 12:47:58 +0100

    add skeleton for basic training and running of agent

--- maze/run.py
+++ maze/run.py
@@ -1,5 +1,16 @@
-import display
-import read_maze
+from maze.lib import read_maze
+import agent
+
+import numpy as np
+
+read_maze.load_maze('./mazes/final.npy')
+walls = read_maze.maze_cells[:, :, 0]
 
 if __name__ == '__main__':
-    pass # todo - run RL
+    a0 = agent.Agent()  # instantiate new agent
+    p0 = a0.run(walls)
+
+    # todo - plot untrained agent path
+
+    a1 = a0.train()  # todo - train agent with one run
+    p1 = a1.run()  # todo - run trained agent
