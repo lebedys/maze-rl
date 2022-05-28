83387042ddc49803cee2e1a795614a7f43d98d61
Adomas Lebedys <adomas.lebedys@protonmail.com>
2022-05-28 17:04:47 +0100

    reformat file

--- maze/lib/util.py
+++ maze/lib/util.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+
 def walls_to_text(walls, nrows=3, ncols=3, row=0, col=0):
     txt = ''
-    for row in walls[row:row+nrows,col:col+ncols]:
+    for row in walls[row:row + nrows, col:col + ncols]:
         for cell in row:
             mark = '-' if cell == 1 else 'x'
             txt += mark + ' '
@@ -10,20 +11,24 @@ def walls_to_text(walls, nrows=3, ncols=3, row=0, col=0):
 
     return txt
 
+
 def fires_to_text(fires, nrows=3, ncols=3, row=0, col=0):
     txt = ''
-    for row in fires[:nrows,:ncols]:
+    for row in fires[:nrows, :ncols]:
         for cell in row:
             txt += str(cell) + ' '
         txt += '\n'
 
     return txt
 
+
 def euclidian_cost(position, goal) -> np.float:
     return np.linalg.norm(goal - position)
 
-def is_wall(direction, walls) -> bool:
-    return False
 
-def is_fire(direction, fires) -> bool:
+def is_wall(cell) -> bool:
+    return cell == 0.0
+
+
+def is_fire(cell) -> bool:
     return False
