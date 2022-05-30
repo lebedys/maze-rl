b52acbb57d95f21c2b754487d2e81999e1b24aca
Adomas Lebedys <adomas.lebedys@protonmail.com>
2022-05-30 12:23:31 +0100

    add logging function skeleton

---
+++ maze/lib/log.py
@@ -0,0 +1,46 @@
+from maze.agent import Agent
+import os
+from datetime import datetime
+
+
+def log_agent(agent: Agent, epoch: int, log_dir: str = 'log/') -> None:
+    # get absolute directory path
+    log_dir_path = os.path.abspath(log_dir)
+    if not os.path.exists(log_dir_path):
+        os.makedirs(log_dir_path)
+
+    # file uses date/time to generate unique name
+    datetime_obj = datetime.now()
+    time_stamp = datetime_obj.strftime("%Y%d%m_%H%M%S")
+
+    # generate file name:
+    file_name = 'log_' + time_stamp + '.log'
+    log_file_path = os.path.join(log_dir_path, file_name)
+
+    # string representations of epoch information
+    agent_params = ''
+    agent_hyperparams = ''
+    agent_history = ''
+
+    # todo - generate strings above
+
+    # appending is used to prevent accidental deletion
+    with open(log_file_path, 'a+') as f:
+        f.write('----------------------\n')
+        f.write('EPOCH {}\n'.format(epoch))
+        f.write('----------------------\n')
+
+        f.write('\nAGENT PARAMETERS\n')
+        f.write(agent_params)
+
+        f.write('\nAGENT HYPER-PARAMETERS\n')
+        f.write(agent_hyperparams)
+
+        f.write('\nAGENT HISTORY\n')
+        f.write(agent_history)
+
+
+if __name__ == '__main__':
+    a0 = Agent
+
+    log_agent(a0, epoch=0)
